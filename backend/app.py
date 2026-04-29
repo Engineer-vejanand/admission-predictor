c18bfbd31b7c09ea1eac71c02b87905c0e95c426
Engineer-vejanand <vejanand04@gmail.com>
2026-04-29 10:29:42 +0530

    Updated app.py for deployment (PORT fix)

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS   # ✅ ADD THIS
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 CORS(app)   # ✅ ENABLE CORS
@@ -53,4 +54,5 @@ def predict():
 
 if __name__ == "__main__":
     print("Server starting...")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
